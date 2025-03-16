fa4333109272570ee1da17ae942c74e930b8c716
Mario Augusto de Lima <marioaulima15@outlook.com>
2025-03-16 19:54:40 -0300

    feat(server): add new keys to env config

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  COOKIE_SECRET: z.string(),
   STRIPE_SECRET_KEY: z.string(),
   SUPABASE_KEY: z.string(),
   SUPABASE_URL: z.string().url(),
