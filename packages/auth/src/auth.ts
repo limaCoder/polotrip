4d8673f45441b23b38ad9b8f365db827b32e122f
Mario Augusto de Lima <marioaulima15@outlook.com>
2025-05-10 00:14:21 -0300

    chore(prd): prepare application to production

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,6 +1,5 @@
 import { type BetterAuthOptions, betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { nextCookies } from 'better-auth/next-js';
 
 import { db } from '@polotrip/db';
 import { schema } from '@polotrip/db/schema';
@@ -56,7 +55,6 @@ export const config: BetterAuthOptions = {
     },
   },
   basePath: '/api/v1/auth',
-  plugins: [nextCookies()],
 };
 
 const auth = betterAuth(config);
