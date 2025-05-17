9ad1a27b27e0e48761529ac6e6fb2184ae4fd4ce
Mario Augusto de Lima <marioaulima15@outlook.com>
2025-05-17 11:30:02 -0300

    feat(web): put loading.tsx in all pages

---
+++ apps/web/src/app/[locale]/(public)/privacy-policy/loading.tsx
@@ -0,0 +1,7 @@
+export default function AlbumLoading() {
+  return (
+    <div className="h-screen w-full py-20 flex items-center justify-center">
+      <div className="w-8 h-8 border-2 border-primary border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
